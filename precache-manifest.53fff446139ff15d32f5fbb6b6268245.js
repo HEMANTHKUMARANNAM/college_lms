f899231e51e79637f146ebe0fb6733cadd32ebcb
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2024-09-06 21:52:38 +0530

    Updates

---
+++ precache-manifest.53fff446139ff15d32f5fbb6b6268245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4487d3db8cb2deedd5dc1cd3a27d795a",
+    "url": "/college_lms/index.html"
+  },
+  {
+    "revision": "b5e04987212550ce2a08",
+    "url": "/college_lms/static/css/main.60d7d2ed.chunk.css"
+  },
+  {
+    "revision": "30a30413fe22c26c82bb",
+    "url": "/college_lms/static/js/2.dc5b95ac.chunk.js"
+  },
+  {
+    "revision": "9a3f2d100d176efad8d0ac01de64801b",
+    "url": "/college_lms/static/js/2.dc5b95ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12cdf2d3859c830512e",
+    "url": "/college_lms/static/js/3.4c085501.chunk.js"
+  },
+  {
+    "revision": "b5e04987212550ce2a08",
+    "url": "/college_lms/static/js/main.fdb02304.chunk.js"
+  },
+  {
+    "revision": "73d7eddeb2ac0caf1f18",
+    "url": "/college_lms/static/js/runtime-main.465670cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/college_lms/precache-manifest.746567b792391d09fee77e6f7605d112.js"
+  "/college_lms/precache-manifest.53fff446139ff15d32f5fbb6b6268245.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/college_lms/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
