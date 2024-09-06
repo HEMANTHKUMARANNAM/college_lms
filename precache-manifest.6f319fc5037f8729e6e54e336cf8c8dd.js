e8eb5ecc5d8e907c9cccf9311f9560e3458f5147
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2024-09-06 21:15:50 +0530

    Updates

---
+++ precache-manifest.6f319fc5037f8729e6e54e336cf8c8dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7348381800e5c2518a8439053cc0a6b3",
+    "url": "/college_lms/index.html"
+  },
+  {
+    "revision": "f588d777e9ce8d311cf8",
+    "url": "/college_lms/static/css/main.60d7d2ed.chunk.css"
+  },
+  {
+    "revision": "944c4c56574323b42aec",
+    "url": "/college_lms/static/js/2.3026cd0f.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/college_lms/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "f588d777e9ce8d311cf8",
+    "url": "/college_lms/static/js/main.cb2034b7.chunk.js"
+  },
+  {
+    "revision": "18eacf992c4647ca080d",
+    "url": "/college_lms/static/js/runtime~main.b454f3a0.js"
+  }
+]);
